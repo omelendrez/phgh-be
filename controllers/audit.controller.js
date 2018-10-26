1576051fbb078379b2ad995fe4e93700fd17a419
omar.melendrez <omar.melendrez@globant.com>
2018-10-26 14:40:42 -0300

    Re-factor backend

--- controllers/audit.controller.js
+++ controllers/audit.controller.js
@@ -1,20 +1,25 @@
 const { Audit } = require('../models')
 const { ReE } = require('../services/util.service')
 
-const create = async function (data) {
+const create = async function(data) {
   Audit.create(data)
 }
 module.exports.create = create
 
 const getAll = (req, res) => {
+  const page = parseInt(req.query.page) - 1
   const query =
-    'SELECT `id`, `userId`, `model`, `recordId`, `field`, `value`, `createdAt`, `updatedAt` FROM `audits` ORDER BY id DESC; '
+    'SELECT `id`, `userId`, `model`, `recordId`, `field`, `value`, `createdAt`, `updatedAt` FROM `audits` ORDER BY id DESC LIMIT ' +
+    (page * 10).toString() +
+    ',10;'
   const result = { success: true }
   Audit.sequelize
     .query(query)
     .then(data => {
-      Object.assign(result, { audit: data[0] })
-      res.json(result)
+      Audit.sequelize.query('SELECT COUNT(0) as rows FROM `audits`;').then(rows => {
+        Object.assign(result, { audit: data[0], rows: rows[0][0].rows })
+        res.json(result)
+      })
     })
     .catch(err => ReE(res, err))
 }
--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const db = {}
-const CONFIG = require('../config/config')
+const CONFIG = require('../config')
 
 const sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, CONFIG.db_password, {
   host: CONFIG.db_host,
