50069868472cc3156b8579cfce0cdafc44a12048
omelendrez <omar.melendrez@gmail.com>
2018-11-03 20:00:46 -0300

    Fix linter complaints

--- services/auth.service.js
+++ services/auth.service.js
@@ -48,7 +48,7 @@ const createParticipant = async (participantInfo) => {
 
   unique_key = participantInfo.username
   if (validator.isAlphanumeric(unique_key)) {
-    auth_info.method = 'username'
+    auth_info.method = 'username';
     [err, participant] = await to(Participant.create(participantInfo))
     if (err) TE(err.message)
     return participant
@@ -58,7 +58,7 @@ const createParticipant = async (participantInfo) => {
 
   unique_key = participantInfo.email
   if (validator.isEmail(unique_key)) {
-    auth_info.method = 'email'
+    auth_info.method = 'email';
     [err, participant] = await to(Participant.create(participantInfo))
     if (err) TE(err.message)
     return participant
@@ -68,7 +68,7 @@ const createParticipant = async (participantInfo) => {
 
   unique_key = participantInfo.phone
   if (validator.isMobilePhone(unique_key, 'any')) { // checks if only phone number was sent
-    auth_info.method = 'phone'
+    auth_info.method = 'phone';
     [err, participant] = await to(Participant.create(participantInfo))
     if (err) TE('Participant already exists with that phone number')
     return participant
