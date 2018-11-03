130137d8895400feee44bce91ad506cfd7c62434
omelendrez <omar.melendrez@gmail.com>
2018-11-03 18:02:17 -0300

    Fix linter detected error

--- controllers/participant.controller.js
+++ controllers/participant.controller.js
@@ -5,8 +5,8 @@ const { Participant } = require('../models')
 
 const create = async function (req, res) {
   const body = req.body
-  if (!body.unique_key && !body.phone && !body.phone) {
-    return ReE(res, 'Please enter an phone or phone number to register.')
+  if (!body.unique_key && !body.email && !body.phone) {
+    return ReE(res, 'Please enter a username or phone number to register.')
   } else if (!body.password) {
     return ReE(res, 'Please enter a password to register.')
   } else {
