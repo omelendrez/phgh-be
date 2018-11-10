2b524a32db814e594b101ad36deb348cdd607298
omelendrez <omar.melendrez@gmail.com>
2018-11-09 22:33:04 -0300

    Add local database config

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ CONFIG.port = process.env.PORT || '3000'
 CONFIG.db_dialect = process.env.DB_DIALECT || 'mysql'
 CONFIG.db_host = process.env.DB_HOST || 'localhost'
 CONFIG.db_port = process.env.DB_PORT || '3306'
-CONFIG.db_name = process.env.DB_NAME || 'phgh'
+CONFIG.db_name = process.env.DB_NAME || 'heroku_945601787818cea'
 CONFIG.db_user = process.env.DB_USER || 'root'
 CONFIG.db_password = process.env.DB_PASSWORD || ''
 
